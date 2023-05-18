c6f881845c21f73cce2e171920c1e604cdd4ff5f
shkurkoka <shkurkoka@gmail.com>
2023-05-18 19:03:14 +0300

    Finish

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,41 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+galleryItems.forEach((item, index) => {
+
+    const li = document.createElement("li");
+    li.classList.add("gallery__item");
+
+    const link = document.createElement("a");
+    link.classList.add("gallery__link");
+    link.href = item.original;
+
+    const newImage = document.createElement("img");
+    newImage.classList.add("gallery__image");
+    newImage.src = item.preview;
+    newImage.setAttribute("data-source", item.original);
+    newImage.alt = item.description;
+
+    link.append(newImage);
+    li.append(link);
+    gallery.append(li);
+});
+
+
+gallery.addEventListener("click", (event) => {
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+
+    event.preventDefault();
+
+    const url = event.target.parentNode.href;
+
+    const instance = basicLightbox.create(`
+        <img src="${url}" />
+    `);
+
+    instance.show();
+} );
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,37 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+galleryItems.forEach((item, index) => {
+
+    const li = document.createElement("li");
+    li.classList.add("gallery__item");
+
+    const link = document.createElement("a");
+    link.classList.add("gallery__link");
+    link.href = item.original;
+
+    const newImage = document.createElement("img");
+    newImage.classList.add("gallery__image");
+    newImage.src = item.preview;
+    newImage.alt = item.description;
+
+    link.append(newImage);
+    li.append(link);
+    gallery.append(li);
+});
+
+const lightbox = new SimpleLightbox('.gallery a');
+
+gallery.addEventListener("click", (event) => {
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+
+    event.preventDefault();
+
+    const url = event.target.parentNode.href;
+
+    return SimpleLightbox('.gallery a');
+} );
